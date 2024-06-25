7401c81159c53abec2ee0f2fc19eda3a31249130
Mariano <marianohilario@gmail.com>
2024-06-25 15:45:52 -0300

    HW3 adding morgan middleware

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import indexRouter from "./routes/indexRouter";
+import morgan from 'morgan'
 
 const server = express();
+server.use(morgan('dev'))
 server.use(express.json());
 server.use(indexRouter)
 
