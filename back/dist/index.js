e174a771c06f5d34d2222dc0b3b53ca8c755f621
marianohilario <marianohilario@gmail.com>
2024-10-13 15:21:07 -0300

    modified 2 for deploy

--- back/dist/index.js
+++ back/dist/index.js
@@ -1,15 +1,43 @@
 "use strict";
+// import server from "./server";
+// import { PORT } from "./config/envs";
+// import "reflect-metadata";
+// import { AppDataSource } from "./config/data-source";
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = __importDefault(require("./server"));
+// AppDataSource.initialize().then(() => {
+//     console.log("Database connection successful");
+//     server.listen(PORT, () => {
+//         console.log(`Server listening on port ${PORT}`);
+//     });
+// });
+const express_1 = __importDefault(require("express"));
+const morgan_1 = __importDefault(require("morgan"));
+const cors_1 = __importDefault(require("cors"));
 const envs_1 = require("./config/envs");
-require("reflect-metadata");
 const data_source_1 = require("./config/data-source");
-data_source_1.AppDataSource.initialize().then(() => {
+const indexRouter_1 = __importDefault(require("./routes/indexRouter"));
+const path_1 = __importDefault(require("path"));
+const server = (0, express_1.default)();
+server.use((0, morgan_1.default)("dev"));
+server.use((0, cors_1.default)());
+server.use(express_1.default.static(path_1.default.join(__dirname, 'public')));
+server.use('/uploads', express_1.default.static(path_1.default.join(__dirname, 'uploads')));
+server.use(express_1.default.json());
+server.use(indexRouter_1.default);
+server.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.message = err.message || "An unexpected error occurred";
+    res.status(err.statusCode).json({ message: err.message });
+});
+data_source_1.AppDataSource.initialize()
+    .then(() => {
     console.log("Database connection successful");
-    server_1.default.listen(envs_1.PORT, () => {
+    server.listen(envs_1.PORT, () => {
         console.log(`Server listening on port ${envs_1.PORT}`);
     });
-});
+})
+    .catch((error) => console.error("Database connection failed:", error));
+exports.default = server;
--- back/src/index.ts
+++ back/src/index.ts
@@ -1,11 +1,47 @@
-import server from "./server";
-import { PORT } from "./config/envs";
-import "reflect-metadata";
-import { AppDataSource } from "./config/data-source";
+// import server from "./server";
+// import { PORT } from "./config/envs";
+// import "reflect-metadata";
+// import { AppDataSource } from "./config/data-source";
 
-AppDataSource.initialize().then(() => {
-    console.log("Database connection successful");
-    server.listen(PORT, () => {
-        console.log(`Server listening on port ${PORT}`);
-    });
+// AppDataSource.initialize().then(() => {
+//     console.log("Database connection successful");
+//     server.listen(PORT, () => {
+//         console.log(`Server listening on port ${PORT}`);
+//     });
+// });
+
+import express, { NextFunction, Request, Response } from "express";
+import morgan from "morgan";
+import cors from "cors";
+import { PORT } from "./config/envs";
+import { AppDataSource } from "./config/data-source";
+import indexRouter from "./routes/indexRouter";
+import AuxError from "./utils/AuxiliarError";
+import path from 'path';
+
+const server = express();
+
+server.use(morgan("dev"));
+server.use(cors());
+server.use(express.static(path.join(__dirname, 'public')));
+server.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+server.use(express.json());
+server.use(indexRouter);
+
+server.use((err: AuxError, req: Request, res: Response, next: NextFunction) => {
+    err.statusCode = err.statusCode || 500;
+    err.message = err.message || "An unexpected error occurred";
+    res.status(err.statusCode).json({ message: err.message });
 });
+
+AppDataSource.initialize()
+    .then(() => {
+        console.log("Database connection successful");
+        server.listen(PORT, () => {
+            console.log(`Server listening on port ${PORT}`);
+        });
+    })
+    .catch((error) => console.error("Database connection failed:", error));
+
+export default server;
+
